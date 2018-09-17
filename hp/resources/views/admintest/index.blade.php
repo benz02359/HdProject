ac3cf08aa1a156c1dfaaaa61aa3aa5959d995803
benz02359 <grean_zone@hotmail.com>
2018-09-17 18:12:27 +0700

    add firebase

---
+++ hp/resources/views/admintest/index.blade.php
@@ -0,0 +1,36 @@
+<head>
+  <title>Firebase Authentication</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+</head>
+ 
+<body>
+<div class="mdl-layout mdl-js-layout">
+  <header class="mdl-layout__header mdl-layout__header--scroll">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="mdl-layout-title">Signup & Login</span>
+      
+    </div>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title">Menu</span>
+    <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="">Link</a>
+      <a class="mdl-navigation__link" href="">Link</a>
+      <a class="mdl-navigation__link" href="">Link</a>
+      <a class="mdl-navigation__link" href="">Link</a>
+    </nav>
+  </div>
+  <main class="mdl-layout__content">
+    <div class="page-content" style="padding-top: 60px;" align="center">
+    <!-- Your content goes here -->
+      
+      
+    </div>
+  </main>
+</div>
+</body>
+ 
+</html>
